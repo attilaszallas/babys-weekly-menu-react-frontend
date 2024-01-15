af58c039e02f530878aa7453578c27177d1958fb
Attila Szallas <ASzallas@slb.com>
2024-01-15 13:21:26 +0100

    Add weekday components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import './App.css';
+import Monday from './Monday';
+import Tuesday from './Tuesday';
+import Wednesday from './Wednesday';
+import Thursday from './Thursday';
+import Friday from './Friday';
+import Saturday from './Saturday';
+import Sunday from './Sunday';
+
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto">
+        <p className="text-2xl text-blue-700 font-bold h-24 hover:text-black py-4">Baby's weekly menu</p>
+          <div className="grid grid-cols-7 grid-rows-7 gap-4">
+            <Monday/>
+            <Tuesday/>
+            <Wednesday/>
+            <Thursday/>
+            <Friday/>
+            <Saturday/>
+            <Sunday/>
+          </div>
+      </div>
     </div>
   );
 }
---
+++ src/Sunday.js
@@ -0,0 +1,52 @@
+
+import React, { useState } from 'react';
+import './App.css';
+
+function Sunday() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem(){
+    console.log(newItem);
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    }
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
+  }
+
+  function deleteItem(id){
+    console.log(id);
+
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
+  return (
+    <div className="App bg-pink-200 rounded">
+      <h1 className='text-xl text-blue-700 font-bold h-10'>Sunday</h1>
+
+      <input
+        type="text"
+        placeholder="Add new item ..."
+        value={newItem}
+        onChange={e=>setNewItem(e.target.value)}
+        className='w-32 border-2 rounded'
+      />
+
+      <button onClick={()=>addItem()} className='bg-blue-500 text-white font-bold py-1 px-4 rounded'>Add</button>
+
+      <ul>
+        {items.map(item =>
+          <li key={item.id}>{item.value} <button className='text-red-700' onClick={()=>deleteItem(item.id)}>X</button></li> 
+          )}
+      </ul>
+
+    </div>
+  );
+}
+
+export default Sunday;
